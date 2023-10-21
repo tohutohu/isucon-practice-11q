cea2a715c62fbaec4c1899ff7c7e48946950f692
tohutohu <tohu.soy@gmail.com>
2023-10-21 13:20:11 +0900

    jia_isu_uuid, timestamp descのインデックス追加

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -25,6 +25,7 @@ CREATE TABLE `isu_condition` (
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+CREATE INDEX jia_isu_uuid_timestamp ON isu_condition (jia_isu_uuid, timestamp DESC);
 
 CREATE TABLE `user` (
   `jia_user_id` VARCHAR(255) PRIMARY KEY,
